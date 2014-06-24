b5f4db528a38323a68e9ff1d993b6aed2815ff8b
Steven Haryanto <stevenharyanto@gmail.com>
2014-06-24 08:35:25 +0000

    Import of SHARYANTO/App-ProgUtils-0.01 from CPAN.
    gitpan-cpan-distribution: App-ProgUtils
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/App-ProgUtils-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/App/ProgUtils.pm
@@ -0,0 +1,66 @@
+package App::ProgUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01'; # VERSION
+our $DATE = '2014-06-24'; # DATE
+
+1;
+# ABSTRACT: Command line to manipulate programs in PATH
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::ProgUtils - Command line to manipulate programs in PATH
+
+=head1 VERSION
+
+This document describes version 0.01 of App::ProgUtils (from Perl distribution App-ProgUtils), released on 2014-06-24.
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+ progless
+ progedit
+ progman
+
+=head1 SEE ALSO
+
+B<which>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-ProgUtils>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-App-ProgUtils>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-ProgUtils>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
