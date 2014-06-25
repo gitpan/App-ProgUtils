5dfd674e0513ca5d66aeb34575bf3201606bd5a2
Steven Haryanto <stevenharyanto@gmail.com>
2014-06-25 05:28:56 +0000

    Import of SHARYANTO/App-ProgUtils-0.02 from CPAN.
    gitpan-cpan-distribution: App-ProgUtils
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/App-ProgUtils-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/App/ProgUtils.pm
+++ lib/App/ProgUtils.pm
@@ -4,8 +4,8 @@ use 5.010001;
 use strict;
 use warnings;
 
-our $VERSION = '0.01'; # VERSION
-our $DATE = '2014-06-24'; # DATE
+our $VERSION = '0.02'; # VERSION
+our $DATE = '2014-06-25'; # DATE
 
 1;
 # ABSTRACT: Command line to manipulate programs in PATH
@@ -22,7 +22,7 @@ App::ProgUtils - Command line to manipulate programs in PATH
 
 =head1 VERSION
 
-This document describes version 0.01 of App::ProgUtils (from Perl distribution App-ProgUtils), released on 2014-06-24.
+This document describes version 0.02 of App::ProgUtils (from Perl distribution App-ProgUtils), released on 2014-06-25.
 
 =head1 SYNOPSIS
 
